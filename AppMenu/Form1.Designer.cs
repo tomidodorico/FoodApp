f5c7545c7bbbfd1cb9538e9062a9e8a65087cd5f
tomidodorico <dodoricotomi@gmail.com>
2022-10-11 23:17:58 -0300

    Creado de clases con sus atributos

---
+++ AppMenu/Form1.Designer.cs
@@ -0,0 +1,65 @@
+﻿
+namespace AppMenu
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(87, 66);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(92, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Ingresa tu nombre";
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(279, 279);
+            this.Controls.Add(this.label2);
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label lblDni;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnAvanzar;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ AppMenu/Local.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppMenu
+{
+    class Local
+    {
+        List<Cliente> clientes = new List<Cliente>();
+        string nombre;
+    }
+}
